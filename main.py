c830c54eb9998cb59e0b54e8e376431da717b3ec
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2025-05-10 21:39:36 +0530

    Added some notes about the code

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from fastapi import FastAPI
 # create an instance of the FastAPI
 app = FastAPI()
 
+
+# we are defining the path and get is called as operation and @app decorator is called as path operation decorator
 @app.get('/')
+#this function index is called as the path operation function
+
 def index():
-    return 'hello world'
+    return {'data':{'name':'Nithin'}}
+
+@app.get('/about')
+def about():
+    return {'data':'about page'}
