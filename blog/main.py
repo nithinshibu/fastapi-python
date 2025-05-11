01e3622848ecf82bc5eeb8f7fcf3abcaa96eb670
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2025-05-11 17:52:08 +0530

    Modified POST API with 201 status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status
 from sqlalchemy.orm import Session
 from blog import schemas,models
 from blog.database import engine,SessionLocal
@@ -16,10 +16,21 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog,db:Session=Depends(get_db)):
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id,db:Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    return blog
