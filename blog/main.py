1268f341bbf44d5b0f2490d5d71914412c02c2aa
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2025-05-11 16:59:32 +0530

    POST API created table values

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Depends
+from sqlalchemy.orm import Session
 from blog import schemas,models
-from blog.database import engine
+from blog.database import engine,SessionLocal
 
 app = FastAPI()
 
@@ -9,7 +9,17 @@ app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return {f'title={request.title} and body={request.body}'}
+def create(request: schemas.Blog,db:Session=Depends(get_db)):
+    new_blog = models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
